682421433509b88be5b08ca373c8079583d16fa4
Sara Baqla <missatrox44@gmail.com>
2025-12-22 08:50:06 -0600

    hide gazetteer

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
     { to: '/', label: 'Home' },
     { to: '/observations', label: 'Observations' },
     { to: '/species', label: 'Species Index' },
-    { to: '/gazetteer', label: 'Gazetteer' },
+    // { to: '/gazetteer', label: 'Gazetteer' },
   ]
 
   return (
