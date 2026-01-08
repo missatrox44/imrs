fe76bf76c61b106f52741b4e9ca024b790782006
Sara Baqla <missatrox44@gmail.com>
2026-01-08 15:45:55 -0600

    species route commented loader

--- src/routes/species.index.tsx
+++ src/routes/species.index.tsx
@@ -2,6 +2,11 @@ import { createFileRoute } from '@tanstack/react-router'
 import SpeciesIndex from '@/components/SpeciesIndex'
 
 export const Route = createFileRoute('/species/')({
+  //   loader: async () => {
+  //   const res = await fetch('/api/species');
+  //   if (!res.ok) throw new Error('Failed to fetch species');
+  //   return res.json();
+  // },
   component: SpeciesIndex,
 })
 
