8fd111136a4d3b1c718e5e1b4a8ecff908878edf
Sara Baqla <missatrox44@gmail.com>
2026-01-12 14:32:37 -0600

    format getCategoryicon

--- src/lib/getCategoryIcon.tsx
+++ src/lib/getCategoryIcon.tsx
@@ -1,29 +1,29 @@
-import { Bird, Bug, Cat, Fish, Leaf, Turtle } from "lucide-react";
-import { GiSuperMushroom } from "react-icons/gi";
-import { LiaFrogSolid } from "react-icons/lia";
-import { LuWorm } from "react-icons/lu";
+import { Bird, Bug, Cat, Fish, Leaf, Turtle } from 'lucide-react'
+import { GiSuperMushroom } from 'react-icons/gi'
+import { LiaFrogSolid } from 'react-icons/lia'
+import { LuWorm } from 'react-icons/lu'
 
 export const getCategoryIcon = (category: string) => {
   switch (category) {
     case 'plants':
-      return <Leaf className="w-4 h-4" />;
+      return <Leaf className="w-4 h-4" />
     case 'birds':
-      return <Bird className="w-4 h-4" />;
+      return <Bird className="w-4 h-4" />
     case 'mammals':
-      return <Cat className="w-4 h-4" />;
+      return <Cat className="w-4 h-4" />
     case 'reptiles':
-      return <Turtle className="w-4 h-4" />;
+      return <Turtle className="w-4 h-4" />
     case 'amphibians':
-      return <LiaFrogSolid className="w-4 h-4" />;
+      return <LiaFrogSolid className="w-4 h-4" />
     case 'arthropods':
-      return <Bug className="w-4 h-4" />;
+      return <Bug className="w-4 h-4" />
     case 'fungi':
-      return <GiSuperMushroom className="w-4 h-4" />;
+      return <GiSuperMushroom className="w-4 h-4" />
     case 'inverts':
       return <LuWorm className="w-4 h-4" />
     case 'fish':
       return <Fish className="w-4 h-4" />
     default:
-      return null;
+      return null
   }
-};
+}
